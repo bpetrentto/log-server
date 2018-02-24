fc2c75d90165ef9a62aa08b955ead0ff01e91b62
bpetrentto <36780197+bpetrentto@users.noreply.github.com>
2018-02-24 04:24:02 +0000

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Ips');
+mongoose.connect('mongodb://bill:/eR_NM{;@ds247058.mlab.com:47058/ips');
 
 module.exports = { mongoose };
